54c86fcd69d135e054c3ba981844468e60be8852
luishuamani <example@example.com>
2023-11-17 17:42:03 -0500

    estructura

---
+++ src/main/java/pe/veterinariasjb/VeterinariaSjbApplication.java
@@ -0,0 +1,13 @@
+package pe.veterinariasjb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinariaSjbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinariaSjbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/veterinariasjb/VeterinariaSjbApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.veterinariasjb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaSjbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
